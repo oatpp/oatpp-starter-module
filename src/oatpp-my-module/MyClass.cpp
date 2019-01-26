a9ec58a661914c16e43c313a5d7fa8597e972a1a
lganzzzo <lganzzzo@gmail.com>
2019-01-26 02:49:22 +0200

    oatpp module bootstrap

---
+++ src/oatpp-my-module/MyClass.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Leonid on 2019-01-26.
+//
+
+#include "MyClass.hpp"
---
+++ src/oatpp-my-module/MyClass.hpp
@@ -0,0 +1,20 @@
+//
+// Created by Leonid on 2019-01-26.
+//
+
+#ifndef PROJECT_MYCLASS_HPP
+#define PROJECT_MYCLASS_HPP
+
+#include "oatpp/core/Types.hpp"
+
+class MyClass {
+public:
+
+  static void doSomething() {
+    OATPP_LOGD("MyClass", "doSomething called");
+  }
+
+};
+
+
+#endif //PROJECT_MYCLASS_H
